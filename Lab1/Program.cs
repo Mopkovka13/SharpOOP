398a4c5ec774c07fb33e29fa344eee1de2f12bfa
Mopkovka13 <evgeniyneskin13@gmail.com>
2022-02-09 23:57:25 +0300

    Lab1 - Done Lab2 - Init Project

---
+++ Lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan StartTime = new TimeSpan(0, 0, 0);
+            string choose;
+            do
+            {
+                PrintMenu();
+                choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "1":
+                        StartTime = stopwatch.Start();
+                        break;
+                    case "2":
+                        stopwatch.Stop(StartTime);
+                        break;
+                    case "3":
+                        stopwatch.ShowTimers();
+                        break;
+                    default:
+                        Console.WriteLine("Нет такого варианта");
+                        break;
+                }
+            } while (choose!="4");
+        }
+
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("1. Включить секундомер\n" +
+                "2. Выключить секундомер\n" +
+                "3. Вывести время по убыванию\n" +
+                "4. Выключить программу\n");
+        }
+    }
+}
---
+++ Lab1/Stopwatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    internal class Stopwatch
+    {
+        private List<TimeSpan> _timers = new List<TimeSpan>();
+        private bool _workStatus = false; //false - выключен / true - включен
+        
+        internal TimeSpan Start()
+        {
+            if(_workStatus==true) // Если уже запущен - исключение
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                _workStatus = true;
+                TimeSpan CurrentTime = DateTime.Now.TimeOfDay;
+                Console.WriteLine("Время начала: {0:hh\\:mm\\:ss}", CurrentTime); //Из-за такого вывода может быть погрешность на 1 секунду, как вариант
+                                                                                  //Console.WriteLine("Время начала: " + CurrentTime); //но тогда будут милисекунды
+                return CurrentTime;
+            }
+        }
+
+        /// <summary>
+        /// Передаём Start, чтобы посчитать разницу во времени
+        /// </summary>
+        internal void Stop(TimeSpan StartTime)
+        {
+            if(_workStatus == false) //Если уже остановлен - исключение
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                _workStatus = false;
+                TimeSpan CurrentTime = DateTime.Now.TimeOfDay;
+                Console.WriteLine("Время окончания: {0:hh\\:mm\\:ss}", CurrentTime);
+                _timers.Add(CurrentTime - StartTime);
+            }
+        }
+
+        internal void ShowTimers()
+        {
+            _timers.Sort();
+            _timers.Reverse();
+            foreach(var timer in _timers)
+            {
+                Console.WriteLine("{0:hh\\:mm\\:ss} ", timer);
+            }
+        }
+    }
+}
